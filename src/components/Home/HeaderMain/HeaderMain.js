5b61a7636b3d2ae1f7b0d84d1bf6a41c6dd433d8
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-04-13 18:28:07 +0600

    main header updated

--- src/components/Home/HeaderMain/HeaderMain.js
+++ src/components/Home/HeaderMain/HeaderMain.js
@@ -1,13 +1,21 @@
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import girl from '../../../images/girl.png';
+import logo from '../../../images/logo.png';
 
 const HeaderMain = () => {
       return (
             <main style={{height:'600px'}} className='row d-flex align-items-center'>
                   <div className="col-md-4 offset-md-1">
-                        <h1 style={{color:'#dee1ec'}}>Your Good Health<br/> Our Responsibility</h1>
-                        <p className='text-secondary'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo aliquam cum iste rem nobis est beatae molestias quis non odio!</p>
-                        <button style={{backgroundColor:'#22d1ee'}} className='btn btn-primary'>Book an Appointment Now</button>
+                        <img src={logo} alt=""/>
+                        <h1 style={{color:'#dee1ec'}} className='text-info'>Your Good Health<br/> Our Responsibility</h1>
+                        <p className='text-white pt-3'>Long term good health comes from taking care of yourself long before a crisis develops. Waiting to do something only after the crisis has arrived is commonly called the "band-aid" approach to feeling good.
+                        <br/>
+                        <br/>
+                        The best way to take back responsibility for your health is to throw away the "band-aid" approach and introduce some prevention. By not relying upon short term fixes for long-term problems, you feel better and are able to enjoy life more. It's your body; it's your health, demand the very best for yourself.
+                        </p>
+                        <button style={{backgroundColor:'#22d1ee'}} className='btn btn-info '>Book an Appointment Now <FontAwesomeIcon icon={faArrowRight} /></button>
                   </div>
                   <div className="col-md-6">
                         <img src={girl} alt="" className="img-fluid"/>
