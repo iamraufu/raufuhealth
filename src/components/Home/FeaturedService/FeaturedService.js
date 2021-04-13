5f0cdacbe0f607e1e657e555ee49e07f8ae71233
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-04-13 18:27:35 +0600

    Featured Service Component added

--- src/components/Home/FeaturedService/FeaturedService.js
+++ src/components/Home/FeaturedService/FeaturedService.js
@@ -1,3 +1,5 @@
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import boy from '../../../images/boy.jpg';
 
@@ -11,8 +13,23 @@ const FeaturedService = () => {
                               </div>
                               <div className="col-md-7 align-self-center">
                                     <h1>Premium Service for <br/> Premium People Like You</h1>
-                                    <p className='text-secondary my-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quod numquam amet consequuntur voluptas? Minima sequi explicabo alias maiores itaque asperiores labore rem officia nulla? Sint laboriosam rem saepe, aut maxime odio doloribus similique vero et eaque impedit, repellendus nam.</p>
-                                    <button style={{backgroundColor:'#22d1ee'}} className='btn btn-primary'>What's More</button>
+                                    <p className='text-secondary my-5'> Our specialist doctors will advise and assist you every step of the way including to home service due to pandemic including the complete dental treatment, checking of all teeth. 
+                                    <br/>
+                                    <br/>
+                                    Our Doctors will also accompany you to your Premium Service appointment on the allocated day and advise you throughout the process.
+                                    <br/>
+                                    <br/>
+                                    It is important to note that the Home treatment will only consider straight forward appointment on the same day. Occasionally, there may be hidden defects with an appointment which will not be possible to identify from the information you provide. 
+                                    <br/>
+                                    <br/>
+                                    If for any reason the Home Doctor refuse your appointment, they may choose not to refund your fees.
+                                    <br/>
+                                    <br/>
+                                    Approximately 95% of all appointment done at the on the same day. We will advise you as to whether your application is straight forward at our initial consultation.
+                                    <br/>
+                                    <br/>
+                                    If you require any further information or if you are happy to proceed with these terms and conditions then please contact us on +8801611404405 or email us at raufu.dev@gmail.com</p>
+                                    <button className='btn btn-info'>What's More <FontAwesomeIcon icon={faArrowRight} /></button>
                               </div>
                         </div>
                   </div>
