6e63582bde3cb341b89823cfa2fcb69eb0fedafb
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-04-13 19:13:07 +0600

    Doctor Component added

---
+++ src/components/Home/Doctor/Doctor.js
@@ -0,0 +1,19 @@
+import { faPhoneAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+
+const Doctor = (props) => {
+      const {img,name,phone} = props.doctor;
+      return (
+            <div className="col-md-4 text-center">
+                  <img className="img-fluid mb-3" src={img} alt=""/>
+                  <div style={{margin:'auto'}} className="">
+
+                  <h4>{name}</h4>
+                  <p><FontAwesomeIcon style={{color:'#22d1ee'}} icon={faPhoneAlt} /> {phone}</p>
+                  </div>
+            </div>
+      );
+};
+
+export default Doctor;
