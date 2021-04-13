e3130d7923b0aece3e25fa6f3ab2442b42c05f4d
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-04-13 19:08:23 +0600

    Blog post component added

---
+++ src/components/Home/BlogPost/BlogPost.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const BlogPost = (props) => {
+      const {title, description, author, authorImg , date} = props.blog;
+      return (
+          <div className="card shadow-sm col-md-4 p-5">
+              <div className="card-header d-flex  align-items-center">
+                  <img className="mx-3" style={{ borderRadius: '50%' }} src={authorImg} alt="" width="60"/>
+                  <div>
+                      <h6 style={{ color: '#22d1ee' }}>{author}</h6>
+                      <p className="m-0 text-secondary">{date}</p>
+                  </div>
+              </div>
+              <div className="card-body">
+                  <h5>{title}</h5>
+                  <p className="card-text blog-post-text text-secondary mt-4">{description}</p>
+              </div>
+              
+         </div>
+      );
+};
+
+export default BlogPost;
