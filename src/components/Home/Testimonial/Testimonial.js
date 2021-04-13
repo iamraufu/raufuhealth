0af49060523ff018a4a280b492c0ea3538f66b5c
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-04-13 19:07:05 +0600

    Testimonial component added

--- src/components/Home/Testimonial/Testimonial.js
+++ src/components/Home/Testimonial/Testimonial.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const Testimonial = (props) => {
-      const { name, from, img } = props.testimonial;
+      const { quote,name, from, img } = props.testimonial;
 
       return (
             <div className="card shadow-sm col-md-4 p-5">
                   <div className="card-body">
-                        <p className="card-text text-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis excepturi numquam facilis quos cumque, fugit explicabo inventore quam expedita illo, sit officia dignissimos ipsum! Id!</p>
+                        <p className="card-text text-secondary">{quote}</p>
                   </div>
                   <div className="card-footer d-flex  align-items-center">
                         <img className="mx-3" style={{ borderRadius: '50%' }} src={img} alt="" width="60" />
