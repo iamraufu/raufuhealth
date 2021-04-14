0bfc62a2f937ec827287eb43e1bbcac97e9531e9
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-04-14 10:46:07 +0600

    Appointment component added

---
+++ src/components/Appointment/Appointment.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../Shared/Navbar/Navbar';
+import Footer from '../Shared/Footer/Footer';
+
+const Appointment = () => {
+      return (
+            <div>
+                 <Navbar/>
+                 <Footer/>
+            </div>
+      );
+};
+
+export default Appointment;
